ac1d4fbf74978653ee81e388bf0d0a374aa5f701
Pathum Sanjana <94284049+PathumSanjana@users.noreply.github.com>
2023-07-04 00:30:28 +0530

    UserController added

---
+++ backend/src/main/java/com/codewithpathum/fullstackbackend/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.codewithpathum.fullstackbackend.controller;
+
+import com.codewithpathum.fullstackbackend.model.User;
+import com.codewithpathum.fullstackbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+}

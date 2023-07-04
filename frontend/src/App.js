25c125b428d9b5698fdb0178bb47f479f21bc293
Pathum Sanjana <94284049+PathumSanjana@users.noreply.github.com>
2023-07-05 01:07:24 +0530

    home page updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import"../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
+      
+        <Navbar/>
+        <Home/>
+      
     </div>
   );
 }

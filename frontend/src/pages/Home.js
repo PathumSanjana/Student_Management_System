1a82309a3a0d9116a63ace7b7a6d2f50f86a802e
Pathum Sanjana <94284049+PathumSanjana@users.noreply.github.com>
2023-07-06 00:29:36 +0530

    connected to the backend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+
+     const [users, setUsers] =useState([]);
+
+     useEffect(() => {
+          loadUsers();
+     }, []);
+
+     const loadUsers =async() => {
+          const result =await axios.get("http://localhost:8081/users");
+          setUsers(result.data);
+          
+     }
+
   return (
     <div className='container'>
         <div className='py-4'>
@@ -8,29 +22,26 @@ export default function Home() {
             <thead>
                 <tr>
                 <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">Name</th>
+                <th scope="col">Username</th>
+                <th scope="col">Email</th>
+                <th scope="col">Action</th>
                 </tr>
             </thead>
             <tbody>
+
+              {
+                users.map((user, index) => (
                 <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
+                <th scope="row" key={index}>{index+1}</th>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
                 </tr>
-                <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-                </tr>
-                <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-                </tr>
+                ))
+              }
+                
+                
             </tbody>
          </table>
         </div>   

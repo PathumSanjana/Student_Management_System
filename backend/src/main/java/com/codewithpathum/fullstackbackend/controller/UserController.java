3cabe36b1adec255a87260101b9fcc9702cbfaf3
Pathum Sanjana <94284049+PathumSanjana@users.noreply.github.com>
2023-07-27 10:56:11 +0530

    edit user logic added

--- backend/src/main/java/com/codewithpathum/fullstackbackend/controller/UserController.java
+++ backend/src/main/java/com/codewithpathum/fullstackbackend/controller/UserController.java
@@ -30,4 +30,15 @@ public class UserController {
       return  userRepository.findById(id)
               .orElseThrow(()->new UserNotFoundException(id));
     }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable long id){
+        return userRepository.findById(id)
+                .map(user -> {
+                     user.setUsername(newUser.getUsername());
+                     user.setName(newUser.getName());
+                     user.setEmail(newUser.getEmail());
+                     return userRepository.save(user);
+                }).orElseThrow(()->new UserNotFoundException(id));
+    }
 }
